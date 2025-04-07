3a8109365267a748047a1425ee201d120525f140
J-RAP <SrRapero720@hotmail.com>
2025-04-06 23:57:46 -0600

    some patches on chromas

--- src/main/java/org/watermedia/videolan4j/tools/Chroma.java
+++ src/main/java/org/watermedia/videolan4j/tools/Chroma.java
@@ -2,6 +2,9 @@ package org.watermedia.videolan4j.tools;
 
 // https://wiki.videolan.org/Chroma/
 public enum Chroma {
+    /**
+     * RGBA is a 32-bit RGBA format in a single plane.
+     */
     RGBA {
         @Override
         public int[] getPitches(int width) {
@@ -13,6 +16,26 @@ public enum Chroma {
             return new int[] { height };
         }
     },
+
+    /**
+     * RV32 is a 24-bit BGR format with 8-bit of padding (no alpha) in a single plane.
+     */
+    RV32 {
+        @Override
+        public int[] getPitches(int width) {
+            return new int[] { width * 4 };
+        }
+
+        @Override
+        public int[] getLines(int height) {
+            return new int[] { height };
+        }
+    },
+
+
+    /**
+     * RV24 is a 24-bit RGB format in a single plane.
+     */
     Rv24 {
         @Override
         public int[] getPitches(int width) {
@@ -24,6 +47,10 @@ public enum Chroma {
             return new int[] { height };
         }
     },
+
+    /**
+     * YUYV is a 16-bit YUV format with 4:2:2 subsampling in a single plane.
+     */
     YUYV {
         @Override
         public int[] getPitches(int width) {
@@ -35,6 +62,10 @@ public enum Chroma {
             return new int[] { height };
         }
     },
+
+    /**
+     * UYVY is a 16-bit YUV format with 4:2:2 subsampling in a single plane.
+     */
     UYVY {
         @Override
         public int[] getPitches(int width) {
@@ -46,6 +77,10 @@ public enum Chroma {
             return new int[] { height };
         }
     },
+
+    /**
+     * GRAW is a 16-bit grayscale format in a single plane.
+     */
     GRAW {
         @Override
         public int[] getPitches(int width) {
@@ -57,6 +92,7 @@ public enum Chroma {
             return new int[] { height };
         }
     },
+
     NV12 {
         @Override
         public int[] getPitches(int width) {
@@ -68,6 +104,7 @@ public enum Chroma {
             return new int[] { height, height / 2 };
         }
     },
+
     I420 {
         @Override
         public int[] getPitches(int width) {
@@ -84,7 +121,7 @@ public enum Chroma {
 
     public abstract int[] getLines(int height);
 
-    public String getChroma() {
-        return this.name();
+    public String canonical() {
+        return this.name().toLowerCase();
     }
 }
